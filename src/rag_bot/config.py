aab4786d2605b028d5d78c18683801c9e3b01967
Nishant Gupta <nishantgupta@Nishants-MacBook-Air.local>
2026-03-12 20:04:32 +0530

    Modified config.py

--- src/rag_bot/config.py
+++ src/rag_bot/config.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+VECTOR_DB_DIR = PROJECT_ROOT / "vector_db"  # adjust path to where your db actually is
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
 CHUNK_SIZE = 1500
@@ -5,8 +8,6 @@ CHUNK_OVERLAP = 200
 
 RETRIEVAL_K = {"k":3}
 
-VECTOR_DB_DIR = "vector_db"
-
-DATA_DIR = "data"
+DATA_DIR = PROJECT_ROOT / "data"
 
 DEFAULT_LLM = "groq"
